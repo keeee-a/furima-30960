a457dbac98fa5a7ca4ee6d9433a90294d1bfbc69
keeee-a <keita.a.2290@icloud.com>
2021-01-20 17:36:04 +0900

    ローカル環境での再現ためのエラー修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
   validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/}
   validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
   validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
-  validates :encrypted_password, uniqueness: true, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :password_confirmation, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
 end
 
