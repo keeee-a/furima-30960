4a07d16debb145552d491ae4985062f37a0a845f
keeee-a <keita.a.2290@icloud.com>
2021-01-21 16:49:48 +0900

    productsテーブルの作成

---
+++ db/migrate/20210121074332_create_products.rb
@@ -0,0 +1,16 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :name, null: false
+      t.integer :category_id, null: false
+      t.integer :price, null: false
+      t.text :explanation, null: false
+      t.integer :product_condition_id, null: false
+      t.integer :burden_shipping_id, null: false
+      t.integer :shipping_area_id, null: false
+      t.integer :days_to_ship_id, null: false
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
