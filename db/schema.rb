512b55e52929613b47a3a1f263bc50cbc6f0a389
keeee-a <keita.a.2290@icloud.com>
2021-01-21 19:49:17 +0900

    Revert "productsテーブルの作成"
    This reverts commit 4a07d16debb145552d491ae4985062f37a0a845f.

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_21_074332) do
-
-  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "category_id", null: false
-    t.integer "price", null: false
-    t.text "explanation", null: false
-    t.integer "product_condition_id", null: false
-    t.integer "burden_shipping_id", null: false
-    t.integer "shipping_area_id", null: false
-    t.integer "days_to_ship_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_products_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2021_01_09_020332) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -45,5 +30,4 @@ ActiveRecord::Schema.define(version: 2021_01_21_074332) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "products", "users"
 end
