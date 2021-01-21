a787dc78d3a0673f960cb0fe5ace7c6888c9310a
keeee-a <keita.a.2290@icloud.com>
2021-01-21 20:16:15 +0900

    activestorageの導入とストロングパラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,10 @@ class ItemsController < ApplicationController
   
   def new
   end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :category_id, :price, :explanation, :product_condition_id, :burden_shipping_id, :shipping_area_id, :days_to_ship_id, :image).merge(user_id: current_user.id)
+  end
 end
